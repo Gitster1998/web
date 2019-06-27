73db6abd1bc5f8ad113c8cc219096aab2f0386d0
Gitster1998 <52268581+Gitster1998@users.noreply.github.com>
2019-06-27 11:13:45 +0530

    Update micro.py

--- micro.py
+++ micro.py
@@ -1,1 +1,16 @@
-from app import app
+x=int(input('1 for add\n2 for subtract\n,3 for multiply\n,4 for divide\n,5 for square root'))
+if x==5:
+  l=int(input('enter a number'))
+  print(l**.5)
+else:
+        m,n=input().split()
+        m=int(m)
+        n=int(n)
+        if x==1:
+          print(m+n)
+        elif x==2:
+          print(m-n)
+        elif x==3:
+          print(m*n)
+        elif x==4:
+          print(m/n)
